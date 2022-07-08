0d3b56bcfa6528b9c85729e75892650868870cdb
Vivek sharma <86712377+Viveksh163@users.noreply.github.com>
2022-07-08 22:46:05 +0530

    Add files via upload

---
+++ libaray-management-service/src/main/java/librarymanagementservice/libaraymanagementservice/LibarayManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package librarymanagementservice.libaraymanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibarayManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibarayManagementServiceApplication.class, args);
+	}
+
+}
---
+++ libaray-management-service/src/test/java/librarymanagementservice/libaraymanagementservice/LibarayManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package librarymanagementservice.libaraymanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibarayManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
